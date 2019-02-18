1b4620bcb8f6bdf4b877d661e5c14f05a33265d4
Vicsante Tangpos Aseniero <vaseniero@gmail.com>
2019-02-19 03:22:28 +0800

    changes in scrape and save examinee passers

--- passers/app/Passer.php
+++ passers/app/Passer.php
@@ -28,6 +28,9 @@ class Passer extends Model
 
     public function __construct(array $attributes = [])
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('passers')->truncate();
+
         $this->row = 0;
         $this->col = 0;
         $this->obj = array();
@@ -39,10 +42,57 @@ class Passer extends Model
 
         $crawler->filter('.container_list div .border_list')->each(function ($node) {
             $item = $node->text();
-            $this->dump_scrape($item,$this->row,$this->col,$this->obj);
+
+            $this->dump_scrape_and_save($item,$this->col,$this->obj);
+
+            // long process of scrape and save
+            // comment out
+            //$this->dump_scrape($item,$this->row,$this->col,$this->obj);
         });
 
-        $this->save_dump_scrape();
+        // long process of scrape and save
+        // comment out
+        //$this->save_dump_scrape();
+    }
+
+    private function dump_scrape_and_save($item,$col,$obj)
+    {
+        if (is_numeric($item)) {
+            $obj['examinee_id'] = $item;
+            $col = 1;
+        }
+        else {
+            switch ($col) {
+                case 1:
+                    $obj['name_of_examinee'] = $item;
+                    $col++;
+                    break;
+                case 2:
+                    $obj['campus_eligibility'] = $item;
+                    $col++;
+                    break;
+                case 3:
+                    $obj['school'] = $item;
+                    $col++;
+                    break;
+                case 4:
+                    $obj['division'] = $item;
+                    $col++;
+                    break;
+                default:
+                    break;
+            }                
+        }
+
+        if ($col == 5) {
+            DB::table('passers')->insert($obj);
+            $this->col = 0;
+            $this->obj = array();
+        }
+        else {
+            $this->col = $col;
+            $this->obj = $obj;
+        }
     }
 
     private function dump_scrape($item,$row,$col,$obj)
@@ -87,9 +137,6 @@ class Passer extends Model
 
     private function save_dump_scrape()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0');
-        DB::table('passers')->truncate();
-
         foreach($this->obj as $record) {
 
             $items = array();
